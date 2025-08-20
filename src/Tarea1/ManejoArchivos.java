7691346928ff34d6a7c730a81f51ed64a6eb2988
AaMmFfEe <alehtsefelixe@gmail.com>
2025-08-20 02:16:04 -0700

    Primer push

---
+++ src/Tarea1/ManejoArchivos.java
@@ -0,0 +1,40 @@
+package Tarea1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ManejoArchivos {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        try {
+
+            File archivo = new File("codigos_postales_hmo.csv");
+            Scanner k = new Scanner(archivo);
+            String texto = k.nextLine();
+
+           while ((texto = k.nextLine()) != null) {
+
+                System.out.println(texto.substring(0,5));
+
+           }
+
+           //CÓDIGO VIEJO // do {
+               // System.out.println(texto.substring(0,5));
+           // }
+
+           // while (k.hasNextLine()); {
+
+            //    System.out.println(texto);
+           //
+        System.out.println ("El código postal ");
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Error con el archivo");
+            e.printStackTrace();
+
+        }
+    }
+}
